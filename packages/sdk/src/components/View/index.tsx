d7e7d8afe37423092424ccca3fb0457ea20eb820
RicardasN <rihaddas@gmail.com>
2022-02-28 14:44:51 +0200

    fix(View): Another fix for typescript issues

--- packages/sdk/src/components/View/index.tsx
+++ packages/sdk/src/components/View/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { View as RNView } from 'react-native';
 import type { ViewProps } from '../../focusManager/types';
 
-const View = React.forwardRef<RNView>((props: ViewProps, ref) => <RNView {...props} ref={ref} />);
+const View = React.forwardRef<RNView, ViewProps>(({ children, ...props }, ref) => (
+  <RNView {...props} ref={ref}>
+    {children}
+  </RNView>
+));
 
 View.displayName = 'View';
 
--- packages/sdk/src/focusManager/types.ts
+++ packages/sdk/src/focusManager/types.ts
@@ -1,11 +1,11 @@
 import type {
-    GestureResponderEvent,
-    ScrollViewProps as RNScrollViewProps,
-    StyleProp,
-    ViewProps as RNViewProps,
-    PressableProps as RNPressableProps,
-    ViewStyle,
-    ScrollView,
+  GestureResponderEvent,
+  ScrollViewProps as RNScrollViewProps,
+  StyleProp,
+  ViewProps as RNViewProps,
+  PressableProps as RNPressableProps,
+  ViewStyle,
+  ScrollView,
 } from 'react-native';
 
 export type ForbiddenFocusDirections = 'down' | 'up' | 'left' | 'right';
@@ -13,129 +13,131 @@ export type WindowAlignment = 'both-edge' | 'low-edge';
 export type ScreenStates = 'background' | 'foreground';
 
 export type PressableFocusOptions = {
-    forbiddenFocusDirections?: ForbiddenFocusDirections[];
-    animatorOptions?: any;
-    focusKey?: string;
-    hasInitialFocus?: boolean;
+  forbiddenFocusDirections?: ForbiddenFocusDirections[];
+  animatorOptions?: any;
+  focusKey?: string;
+  hasInitialFocus?: boolean;
 };
 
 export type ScreenFocusOptions = {
-    forbiddenFocusDirections?: ForbiddenFocusDirections[];
-    focusKey?: string;
-    verticalWindowAlignment?: WindowAlignment;
-    horizontalWindowAlignment?: WindowAlignment;
-    nextFocusLeft?: string | string[];
-    nextFocusRight?: string | string[];
-    nextFocusUp?: string | string[];
-    nextFocusDown?: string | string[];
+  forbiddenFocusDirections?: ForbiddenFocusDirections[];
+  focusKey?: string;
+  verticalWindowAlignment?: WindowAlignment;
+  horizontalWindowAlignment?: WindowAlignment;
+  nextFocusLeft?: string | string[];
+  nextFocusRight?: string | string[];
+  nextFocusUp?: string | string[];
+  nextFocusDown?: string | string[];
 };
 
 export type RecyclableListFocusOptions = {
-    forbiddenFocusDirections?: ForbiddenFocusDirections[];
+  forbiddenFocusDirections?: ForbiddenFocusDirections[];
 };
 
 export interface ViewProps extends RNViewProps {
-    focus?: boolean;
-    focusOptions?: PressableFocusOptions;
-    parentContext?: any;
-    repeatContext?: any;
-    onPress?: (e: GestureResponderEvent | any) => void;
-    onBlur?: (response?: any) => void;
-    onFocus?: any;
-    children?: React.ReactNode;
-    ref?: any;
-    activeOpacity?: any;
-    style?: StyleProp<ViewStyle>;
+  focus?: boolean;
+  focusOptions?: PressableFocusOptions;
+  parentContext?: any;
+  repeatContext?: any;
+  onPress?: (e: GestureResponderEvent | any) => void;
+  onBlur?: (response?: any) => void;
+  onFocus?: any;
+  children?: React.ReactNode;
+  ref?: any;
+  activeOpacity?: any;
+  style?: StyleProp<ViewStyle>;
+  animatorOptions?: any;
+  className?: string;
 }
 
 export interface PressableProps extends RNPressableProps {
-    focus?: boolean;
-    focusOptions?: PressableFocusOptions;
-    parentContext?: any;
-    repeatContext?: any;
-    onPress?: (e: GestureResponderEvent | any) => void;
-    onBlur?: (response?: any) => void;
-    onFocus?: any;
-    children: React.ReactNode;
-    ref?: any;
-    activeOpacity?: any;
-    style?: StyleProp<ViewStyle>;
+  focus?: boolean;
+  focusOptions?: PressableFocusOptions;
+  parentContext?: any;
+  repeatContext?: any;
+  onPress?: (e: GestureResponderEvent | any) => void;
+  onBlur?: (response?: any) => void;
+  onFocus?: any;
+  children: React.ReactNode;
+  ref?: any;
+  activeOpacity?: any;
+  style?: StyleProp<ViewStyle>;
 }
 
 export interface ScrollViewProps extends RNScrollViewProps {
-    parentContext?: any;
-    horizontal?: boolean;
-    children?: React.ReactNode;
-    ref?: React.MutableRefObject<ScrollView>;
+  parentContext?: any;
+  horizontal?: boolean;
+  children?: React.ReactNode;
+  ref?: React.MutableRefObject<ScrollView>;
 }
 
 export interface ScreenProps {
-    screenState?: ScreenStates;
-    screenOrder?: number;
-    stealFocus?: boolean;
-    children?: React.ReactNode;
-    style?: StyleProp<ViewStyle>;
-    onBlur?: () => void;
-    onFocus?: () => void;
-    focusOptions?: ScreenFocusOptions;
+  screenState?: ScreenStates;
+  screenOrder?: number;
+  stealFocus?: boolean;
+  children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+  onBlur?: () => void;
+  onFocus?: () => void;
+  focusOptions?: ScreenFocusOptions;
 }
 
 export interface RecyclerViewProps {
-    parentContext?: any;
-    repeatContext?: any;
-    isHorizontal?: boolean;
-    children?: React.ReactNode;
-    dataProvider: any;
-    layoutProvider: any;
-    rowRenderer: any;
-    bounces?: boolean;
-    scrollViewProps?: any;
-    scrollEventThrottle?: number;
-    contentContainerStyle?: StyleProp<ViewStyle>;
-    style?: StyleProp<ViewStyle>;
-    focusOptions?: RecyclableListFocusOptions;
+  parentContext?: any;
+  repeatContext?: any;
+  isHorizontal?: boolean;
+  children?: React.ReactNode;
+  dataProvider: any;
+  layoutProvider: any;
+  rowRenderer: any;
+  bounces?: boolean;
+  scrollViewProps?: any;
+  scrollEventThrottle?: number;
+  contentContainerStyle?: StyleProp<ViewStyle>;
+  style?: StyleProp<ViewStyle>;
+  focusOptions?: RecyclableListFocusOptions;
 }
 
 export interface Context {
-    id: string;
-    type: string;
-    children: Context[];
-    parent?: Context;
-    layout?: any;
-    prevState?: string;
-    screen?: Context;
-    data?: any;
-    onFocus?(): void;
-    onBlur?(): void;
-    isScrollable?: boolean;
-    nodeId?: any;
-    node?: any;
-    isFocused?: boolean;
-    isHorizontal?: boolean;
-    stealFocus?: boolean;
-    verticalWindowAlignment?: string;
-    horizontalWindowAlignment?: string;
-    isNested?: boolean;
-    order?: number;
-    scrollOffsetX?: number;
-    scrollOffsetY?: number;
-    isLastVisible?(): boolean;
-    isFirstVisible?(): boolean;
-    isRecyclable?: boolean;
-    isFocusable?: boolean;
-    focusKey?: string;
-    state?: string; // proper type
-    initialFocus?: Context;
-    lastFocused?: Context;
-    repeatContext?: Context;
-    parentContext?: Context;
-    layouts?: any;
-    index?: number;
-    forbiddenFocusDirections?: string[];
-    nextFocusLeft?: string | string[];
-    nextFocusRight?: string | string[];
-    nextFocusUp?: string | string[];
-    nextFocusDown?: string | string[];
+  id: string;
+  type: string;
+  children: Context[];
+  parent?: Context;
+  layout?: any;
+  prevState?: string;
+  screen?: Context;
+  data?: any;
+  onFocus?(): void;
+  onBlur?(): void;
+  isScrollable?: boolean;
+  nodeId?: any;
+  node?: any;
+  isFocused?: boolean;
+  isHorizontal?: boolean;
+  stealFocus?: boolean;
+  verticalWindowAlignment?: string;
+  horizontalWindowAlignment?: string;
+  isNested?: boolean;
+  order?: number;
+  scrollOffsetX?: number;
+  scrollOffsetY?: number;
+  isLastVisible?(): boolean;
+  isFirstVisible?(): boolean;
+  isRecyclable?: boolean;
+  isFocusable?: boolean;
+  focusKey?: string;
+  state?: string; // proper type
+  initialFocus?: Context;
+  lastFocused?: Context;
+  repeatContext?: Context;
+  parentContext?: Context;
+  layouts?: any;
+  index?: number;
+  forbiddenFocusDirections?: string[];
+  nextFocusLeft?: string | string[];
+  nextFocusRight?: string | string[];
+  nextFocusUp?: string | string[];
+  nextFocusDown?: string | string[];
 }
 
 export type ContextMap = { [key: string]: Context };
