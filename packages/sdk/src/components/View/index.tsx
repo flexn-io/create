bf5978c85601d582820d7fd8622c6d1cc2b126a1
aurimas535 <aurimasmickys@gmail.com>
2022-03-02 10:50:36 +0200

    add dataSet prop to types, fix linting

--- packages/sdk/src/components/View/index.tsx
+++ packages/sdk/src/components/View/index.tsx
@@ -3,9 +3,9 @@ import { View as RNView } from 'react-native';
 import type { ViewProps } from '../../focusManager/types';
 
 const View = React.forwardRef<RNView, ViewProps>(({ children, ...props }, ref) => (
-  <RNView {...props} ref={ref}>
-    {children}
-  </RNView>
+    <RNView {...props} ref={ref}>
+        {children}
+    </RNView>
 ));
 
 View.displayName = 'View';
