9224514bbacbf0d8aa96ab72bfd7e13a71a41f6a
pauliusguzas <paulius.guzas@reactseals.com>
2022-05-03 13:23:12 +0300

    wip: add e2e tests

--- packages/harness/test/specs/e2e.js
+++ packages/harness/test/specs/e2e.js
@@ -4,29 +4,25 @@ describe('Test Harness app', () => {
     before(() => {
         FlexnRunner.launchApp();
     });
-    // if (process.env.PLATFORM === 'macos') {
-    //     it('expect first button to be existing by ID', async () => {
-    //         await FlexnRunner.expectToBeExistingById('flexn-screens-home-test-case-list-button-0-0');
-    //     });
-    // } else {
-    //     it('navigate to rows screen and return to home screen and expect Rows button to be displayed by ID', async () => {
-    //         await FlexnRunner.waitForDisplayedById('flexn-screens-home-test-case-list-button-0-0');
-    //         await FlexnRunner.pressButtonDown(10);
-    //         if (process.env.PLATFORM === 'web') {
-    //             await FlexnRunner.scrollById('flexn-screens-home-test-case-list-button-1-0', 'down');
-    //             await FlexnRunner.clickById('flexn-screens-home-test-case-list-button-1-0');
-    //         } else {
-    //             await FlexnRunner.scrollById('flexn-screens-home-test-case-list-button-2-0', 'down');
-    //             await FlexnRunner.clickById('flexn-screens-home-test-case-list-button-2-0');
-    //             await FlexnRunner.pressButtonSelect(1);
-    //         }
-    //         await FlexnRunner.waitForDisplayedById('flexn-screens-focus-performance-rows-button-0-rows-amount-10');
-    //         await FlexnRunner.pressButtonBack(1);
-    //         await FlexnRunner.expectToBeDisplayedById('flexn-screens-home-test-case-list-button-2-0');
-    //     });
-    // }
-
-    it('expect first button to be existing by ID', async () => {
-        await FlexnRunner.expectToBeExistingById('flexn-screens-home-test-case-list-button-0-0');
-    });
+    if (process.env.PLATFORM === 'macos') {
+        it('expect first button to be existing by ID', async () => {
+            await FlexnRunner.expectToBeExistingById('flexn-screens-home-test-case-list-button-0-0');
+        });
+    } else {
+        it('navigate to rows screen and return to home screen and expect Rows button to be displayed by ID', async () => {
+            await FlexnRunner.waitForDisplayedById('flexn-screens-home-test-case-list-button-0-0');
+            await FlexnRunner.pressButtonDown(10);
+            if (process.env.PLATFORM === 'web') {
+                await FlexnRunner.scrollById('flexn-screens-home-test-case-list-button-1-0', 'down');
+                await FlexnRunner.clickById('flexn-screens-home-test-case-list-button-1-0');
+            } else {
+                await FlexnRunner.scrollById('flexn-screens-home-test-case-list-button-2-0', 'down');
+                await FlexnRunner.clickById('flexn-screens-home-test-case-list-button-2-0');
+                await FlexnRunner.pressButtonSelect(1);
+            }
+            await FlexnRunner.waitForDisplayedById('flexn-screens-focus-performance-rows-button-0-rows-amount-10');
+            await FlexnRunner.pressButtonBack(1);
+            await FlexnRunner.expectToBeDisplayedById('flexn-screens-home-test-case-list-button-2-0');
+        });
+    }
 });
--- packages/template/test/specs/e2e.js
+++ packages/template/test/specs/e2e.js
@@ -5,27 +5,23 @@ describe('Test template', () => {
         FlexnRunner.launchApp();
     });
 
-    // it('--> check if elements exists in Home screen', async () => {
-    //     await FlexnRunner.expectToBeExistingByText('Flexn SDK Example');
-    //     await FlexnRunner.expectToBeExistingById('template-screen-home-try-me-button');
-    //     await FlexnRunner.expectToBeExistingById('template-screen-home-now-try-me-button');
-    //     await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-github');
-    //     await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-renative');
-    //     await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-twitter');
-    // });
-
-    // it('--> click "Try my" button', async () => {
-    //     await FlexnRunner.clickById('template-screen-home-try-me-button');
-    //     await FlexnRunner.pressButtonSelect(1);
-    // });
-    
-    // it('--> click "Now try my" button', async () => {
-    //     await FlexnRunner.pressButtonDown(1);
-    //     await FlexnRunner.clickById('template-screen-home-now-try-me-button');
-    //     await FlexnRunner.pressButtonSelect(1);
-    // });
-
     it('--> check if elements exists in Home screen', async () => {
         await FlexnRunner.expectToBeExistingByText('Flexn SDK Example');
+        await FlexnRunner.expectToBeExistingById('template-screen-home-try-me-button');
+        await FlexnRunner.expectToBeExistingById('template-screen-home-now-try-me-button');
+        await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-github');
+        await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-renative');
+        await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-twitter');
+    });
+
+    it('--> click "Try my" button', async () => {
+        await FlexnRunner.clickById('template-screen-home-try-me-button');
+        await FlexnRunner.pressButtonSelect(1);
+    });
+    
+    it('--> click "Now try my" button', async () => {
+        await FlexnRunner.pressButtonDown(1);
+        await FlexnRunner.clickById('template-screen-home-now-try-me-button');
+        await FlexnRunner.pressButtonSelect(1);
     });
 });
