082eb53ac01d70dc509414a7f73e4ae956f2d72d
aurimasmi <aurimasmickys@gmail.com>
2022-05-04 22:33:34 +0300

    add basic primitive components

---
+++ packages/sdk/src/components/Modal/index.lng.js
@@ -0,0 +1,33 @@
+import Lightning from '@lightningjs/core';
+
+export default class Modal extends Lightning.Component {
+    static _template() {
+        return {
+            rect: true,
+            color: 0xffffffff,
+            w: 0,
+            h: 0,
+        };
+    }
+
+    get content() {
+        return this._content;
+    }
+
+    set content(content) {
+        this._content = content;
+        this._render();
+    }
+
+    _init() {
+        this._render();
+    }
+
+    _render() {
+        this.patch(this.content);
+    }
+
+    _handleBack() {
+        this.signal('onClose');
+    }
+}
---
+++ packages/sdk/src/components/RecyclableList/index.lng.js
@@ -0,0 +1,7 @@
+import { Row } from '@lightningjs/ui-components';
+
+const RecyclableListDataProvider = null;
+const RecyclableListLayoutProvider = null;
+
+export default Row;
+export { RecyclableListDataProvider, RecyclableListLayoutProvider };
