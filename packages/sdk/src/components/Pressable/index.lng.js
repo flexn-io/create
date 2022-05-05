fcab32faf3c4ed3de249fafbb2f231e5028b55b9
aurimasmi <aurimasmickys@gmail.com>
2022-05-05 09:22:26 +0300

    add more primitive components, resurrect harness app lng

---
+++ packages/sdk/src/components/Pressable/index.lng.js
@@ -0,0 +1,31 @@
+import Lightning from '@lightningjs/core';
+
+export default class Pressable extends Lightning.Component {
+    static _template() {
+        return {
+            w: 0,
+            h: 0
+        };
+    }
+
+    get content() {
+        return this._content;
+    }
+
+    set content(content) {
+        this._content = content;
+        this._render();
+    }
+
+    _init() {
+        this._render();
+    }
+
+    _render() {
+        this.patch(this.content);
+    }
+
+    _handleBack() {
+        this.signal('onPress');
+    }
+}
