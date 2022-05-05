8b83f2f402d3ecb5312a7157536a37f6f64e49ef
aurimasmi <aurimasmickys@gmail.com>
2022-05-05 09:58:20 +0300

    add empty components files for sdk to compile in lng

--- packages/harness/src/screens/TestCases/Components/Icon/index.lng.js
+++ packages/harness/src/screens/TestCases/Components/Icon/index.lng.js
@@ -1,9 +1,9 @@
 import Lightning from '@lightningjs/core';
-
+import { Icon as FlexnIcon } from '@flexn/sdk';
 export default class Icon extends Lightning.Component {
     static _template() {
         return {
-            
+            type: FlexnIcon
         };
     }
 }
