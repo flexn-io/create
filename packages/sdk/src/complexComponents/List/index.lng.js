a27358d9cbb0d73354ce9133575324aefe66a281
aurimasmi <aurimasmickys@gmail.com>
2022-05-11 23:53:18 +0300

    List component WIP

---
+++ packages/sdk/src/complexComponents/List/index.lng.js
@@ -0,0 +1,124 @@
+import { Column } from '@lightningjs/ui-components';
+import { Lightning } from '@lightningjs/sdk';
+import Row from '../Row';
+export default class List extends Lightning.Component {
+    static _template() {
+        return {
+            List: {
+                type: Column,
+                itemSpacing: 25,
+                items: [],
+            },
+        };
+    }
+
+    _construct() {
+        this._whenEnabled = new Promise((resolve) => (this._enable = resolve), console.error);
+    }
+
+    get _List() {
+        return this.tag('List');
+    }
+
+    get w() {
+        return this._w;
+    }
+
+    set w(value) {
+        if (value !== this._w) {
+            this._w = value;
+            this.w = value;
+
+            this._whenEnabled.then(() => {
+                this._Row.w = this.w;
+            });
+        }
+    }
+
+    get h() {
+        return this._h;
+    }
+
+    set h(value) {
+        if (value !== this._h) {
+            this._h = value;
+            this.h = value;
+
+            this._whenEnabled.then(() => {
+                this._Row.h = this.h;
+            });
+        }
+    }
+
+    get data() {
+        return this._data;
+    }
+
+    set data(value) {
+        this._data = value;
+        this._whenEnabled.then(() => {
+            this._List.items = this.data.map((rowData) => ({
+                type: Row,
+                data: rowData,
+                focusOptions: this.focusOptions,
+                lazyScroll: this.lazyScroll,
+                ...this.card,
+            }));
+        });
+    }
+
+    get card() {
+        return this._card || {};
+    }
+
+    set card(card) {
+        this._card = card;
+    }
+
+    get focusOptions() {
+        return this._focusOptions;
+    }
+
+    set focusOptions(value) {
+        this._focusOptions = value;
+    }
+
+    get itemSpacing() {
+        return this._itemSpacing;
+    }
+
+    set itemSpacing(value) {
+        if (value !== this._itemSpacing) {
+            this._itemSpacing = value;
+            this._whenEnabled.then(() => {
+                this._List.itemSpacing = value;
+            });
+        }
+    }
+
+    get lazyScroll() {
+        return this._lazyScroll;
+    }
+
+    set lazyScroll(value) {
+        if (value !== this.lazyScroll) {
+            this._lazyScroll = value;
+        }
+    }
+
+    $onCardPress(eventValue) {
+        this.signal('onPress', eventValue);
+    }
+
+    $onCardFocus(eventValue) {
+        this.signal('onFocus', eventValue);
+    }
+
+    $onCardBlur(eventValue) {
+        this.signal('onBlur', eventValue);
+    }
+
+    _getFocused() {
+        return this._List;
+    }
+}
