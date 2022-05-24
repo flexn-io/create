f7b8d900ff8b47afa36a87b3fab3b4ead0060bc7
aurimasmi <aurimasmickys@gmail.com>
2022-05-24 16:09:24 +0300

    fix: add different dimensions for mobile components

--- packages/template/src/screens/carousels.tsx
+++ packages/template/src/screens/carousels.tsx
@@ -11,7 +11,7 @@ const ScreenCarousels = ({ navigation }: { navigation?: any }) => {
     const data = [...Array(10).keys()].map((rowNumber) => {
         return {
             items: getRandomData(rowNumber, undefined),
-            itemsInViewport: interval(3, 5),
+            itemsInViewport: interval(isFactorMobile ? 1 : 3, isFactorMobile ? 3 : 5),
         };
     });
 
@@ -19,11 +19,11 @@ const ScreenCarousels = ({ navigation }: { navigation?: any }) => {
         <Screen style={[theme.styles.screen, styles.screen]}>
             <List
                 items={data}
-                itemDimensions={{ height: 250 }}
-                rowHeight={400}
+                itemDimensions={{ height: isFactorMobile ? 200 : 250 }}
+                rowHeight={isFactorMobile ? 350 : 400}
                 animatorOptions={{ type: 'scale_with_border', scale: 1.1 }}
                 focusOptions={{ forbiddenFocusDirections: ['right'] }}
-                itemSpacing={30}
+                itemSpacing={isFactorMobile ? 15 : 30}
                 cardStyle={styles.cardStyle}
                 onPress={(data) => {
                     navigation.navigate(ROUTES.DETAILS, { row: data.rowNumber, index: data.index });
@@ -35,7 +35,7 @@ const ScreenCarousels = ({ navigation }: { navigation?: any }) => {
 
 const styles = {
     screen: {
-        left: Ratio(100),
+        left: isFactorMobile ? 0 : Ratio(100),
     },
     cardStyle: {
         borderWidth: isFactorMobile ? 0 : 5,
