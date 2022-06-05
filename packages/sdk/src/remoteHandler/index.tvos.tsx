06de7c53c71ecf0230955a35a1afc1e7728bdcb8
aurimasmi <aurimasmickys@gmail.com>
2022-06-05 11:44:05 +0300

    fix: add throtle to tvos events

--- packages/sdk/src/remoteHandler/index.tvos.tsx
+++ packages/sdk/src/remoteHandler/index.tvos.tsx
@@ -1,14 +1,17 @@
-import { useEffect } from 'react';
-
+import { useEffect, useCallback } from 'react';
 import { NativeModules, NativeEventEmitter } from 'react-native';
+import throttle from 'lodash.throttle';
 
 const useTVRemoteHandler = (callback: any) => {
+    const cb = useCallback(throttle(callback, 100), []);
+
     useEffect(() => {
         const { TvRemoteHandler } = NativeModules;
         const eventEmitter = new NativeEventEmitter(TvRemoteHandler);
-
         const executeEvent = (eventData: any) => {
-            callback(eventData);
+            if (eventData.eventKeyAction === 'down') {
+                cb(eventData);
+            }
         };
 
         eventEmitter.addListener('onTVRemoteKey', executeEvent);
