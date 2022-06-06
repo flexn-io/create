ab2036c14abe6512131d166f36fb33c9417fd301
pauliusguzas <paulius.guzas@flexn.io>
2022-06-06 10:40:22 +0300

    chore:update template tests

--- packages/graybox/src/implementations/wdio/helpers.ts
+++ packages/graybox/src/implementations/wdio/helpers.ts
@@ -1,9 +1,17 @@
 /* eslint-disable no-await-in-loop */
+// const pressButtonAndroid = async (n: number, keyCode: number) => {
+//     for (let i = 0; i < n; i++) {
+//         await driver.pressKeyCode(keyCode);
+//         // await browser.pause(500);
+//     }
+// };
+
 const pressButtonAndroid = async (n: number, keyCode: number) => {
+    const promises = [];
     for (let i = 0; i < n; i++) {
-        await driver.pressKeyCode(keyCode);
-        await browser.pause(500);
+        promises[i] = driver.pressKeyCode(keyCode);
     }
+    await Promise.all(promises);
 };
 
 const pressButtonIos = async (n: number, name: string) => {
--- packages/template/test/specs/e2e.js
+++ packages/template/test/specs/e2e.js
@@ -6,20 +6,22 @@ describe('Test template', () => {
     });
 
     it('--> check if elements exists in Home screen', async () => {
-        await FlexnRunner.expectToBeExistingByText('Flexn SDK Example');
-        await FlexnRunner.expectToBeExistingById('template-screen-home-try-me-button');
-        await FlexnRunner.expectToBeExistingById('template-screen-home-now-try-me-button');
+        await FlexnRunner.expectToBeDisplayedByText('Flexn SDK Example');
+        await FlexnRunner.expectToBeDisplayedById('template-screen-home-try-me-button');
+        await FlexnRunner.expectToBeDisplayedById('template-screen-home-now-try-me-button');
         await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-github');
         await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-renative');
         await FlexnRunner.expectToBeExistingById('template-screen-home-navigate-to-twitter');
     });
 
     it('--> click "Try my" button', async () => {
+        await FlexnRunner.expectToBeClickableById('template-screen-home-try-me-button');
         await FlexnRunner.clickById('template-screen-home-try-me-button');
         await FlexnRunner.pressButtonSelect(1);
     });
     
     it('--> click "Now try my" button', async () => {
+        await FlexnRunner.expectToBeClickableById('template-screen-home-now-try-me-button');
         await FlexnRunner.pressButtonDown(1);
         await FlexnRunner.clickById('template-screen-home-now-try-me-button');
         await FlexnRunner.pressButtonSelect(1);
