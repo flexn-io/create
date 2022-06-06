1a2fb7422683ced532b1d8bdf9cda51f970b6697
pauliusguzas <paulius.guzas@flexn.io>
2022-06-06 11:20:05 +0300

    wip:testing atv button clicking

--- packages/graybox/src/implementations/wdio/helpers.ts
+++ packages/graybox/src/implementations/wdio/helpers.ts
@@ -1,9 +1,8 @@
+/* eslint-disable no-await-in-loop */
 const pressButtonAndroid = async (n: number, keyCode: number) => {
-    const promises = [];
     for (let i = 0; i < n; i++) {
-        promises[i] = driver.pressKeyCode(keyCode);
+        await driver.pressKeyCode(keyCode);
     }
-    await Promise.all(promises);
 };
 
 const pressButtonIos = async (n: number, name: string) => {
