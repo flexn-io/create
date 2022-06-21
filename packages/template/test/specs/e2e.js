cf9a66cc32d1d8f4f9c0cdfb6e3b930f52dbbbd9
pauliusguzas <paulius.guzas@flexn.io>
2022-06-21 15:15:39 +0300

    chore: add final test changes

--- packages/template/test/specs/e2e.js
+++ packages/template/test/specs/e2e.js
@@ -5,15 +5,14 @@ describe('Test template', () => {
         FlexnRunner.launchApp();
     });
 
-    it.skip('--> check if elements are displayed in Home Page', async () => {
+    it('--> check if elements are displayed in Home Page', async () => {
         await FlexnRunner.expectToBeDisplayedById('template-home-screen-flexn-image');
         await FlexnRunner.expectToBeDisplayedById('template-home-screen-welcome-message-text');
         await FlexnRunner.expectToBeDisplayedById('template-home-screen-version-number-text');
-        await FlexnRunner.expectToBeDisplayedById('template-home-screen-try-my-button');
-        await FlexnRunner.expectToBeDisplayedById('template-home-screen-now-try-my-button');
+        await FlexnRunner.expectToBeDisplayedById('template-home-screen-try-me-button');
+        await FlexnRunner.expectToBeDisplayedById('template-home-screen-now-try-me-button');
     });
 
-    //tested:web, ios, android, macos
     it('--> check if Carousels Page opens when "Carousels" button is selected', async () => {
         await FlexnRunner.waitForDisplayedById('template-home-screen-flexn-image');
         if (process.env.PLATFORM === 'android' || process.env.PLATFORM === 'ios') {
@@ -28,13 +27,13 @@ describe('Test template', () => {
             await FlexnRunner.clickById('template-menu-drawer-button');
         }
         await FlexnRunner.clickById('template-menu-home-button');
-        // await FlexnRunner.pressButtonLeft(1);
-        // await FlexnRunner.pressButtonSelect(1);
+        await FlexnRunner.pressButtonLeft(1);
+        await FlexnRunner.pressButtonUp(1);
+        await FlexnRunner.pressButtonSelect(1);
         await FlexnRunner.expectToBeDisplayedById('template-home-screen-flexn-image');
     });
 
-    // works on all platforms
-    it.skip('--> check if My Modal opens when "My Modal" button is selected', async () => {
+    it('--> check if My Modal opens when "My Modal" button is selected', async () => {
         await FlexnRunner.waitForDisplayedById('template-home-screen-flexn-image');
         if (process.env.PLATFORM === 'android' || process.env.PLATFORM === 'ios') {
             await FlexnRunner.clickById('template-menu-drawer-button');
@@ -52,27 +51,18 @@ describe('Test template', () => {
         await FlexnRunner.expectToBeDisplayedById('template-home-screen-flexn-image');
     });
 
-    it.skip('--> check if Carousels Page opens when "Now Try Me!" button is selected', async () => {
+    it('--> check if Carousels Page opens when "Now Try Me!" button is selected', async () => {
         await FlexnRunner.waitForDisplayedById('template-home-screen-flexn-image');
-        await FlexnRunner.clickById('template-home-screen-now-try-my-button');
+        await FlexnRunner.clickById('template-home-screen-now-try-me-button');
         await FlexnRunner.pressButtonDown(1);
         await FlexnRunner.pressButtonSelect(1);
-        // should be 2 clicks, 3 are needed for ATV due to bug
-        if (process.env.PLATFORM === 'androidtv') {
-            await FlexnRunner.pressButtonDown(3);
-        } else {
-            await FlexnRunner.pressButtonDown(2);
+        await FlexnRunner.expectToBeDisplayedById('template-carousels-screen-container');
+        if (process.env.PLATFORM === 'android' || process.env.PLATFORM === 'ios') {
+            await FlexnRunner.clickById('template-menu-drawer-button');
         }
-        await FlexnRunner.pressButtonSelect(1);
-        await FlexnRunner.expectToBeDisplayedById('template-starter-my-page-text-container');
-        if (process.env.PLATFORM === 'ios' || process.env.PLATFORM === 'macos') {
-            await FlexnRunner.clickById('header-back');
-        } else if (process.env.PLATFORM === 'android') {
-            await FlexnRunner.clickById('home, back');
-        } else {
-            await FlexnRunner.clickById('template-starter-menu-home-button');
-        }
+        await FlexnRunner.clickById('template-menu-home-button');
         await FlexnRunner.pressButtonLeft(1);
+        await FlexnRunner.pressButtonUp(2);
         await FlexnRunner.pressButtonSelect(1);
         await FlexnRunner.expectToBeDisplayedById('template-home-screen-flexn-image');
     });
