ee599eb6e080348e5c363341573db73c9e9c81e5
pauliusguzas <paulius.guzas@flexn.io>
2022-06-22 10:26:56 +0300

    chore: update graybox version

--- packages/harness/test/specs/e2e.js
+++ packages/harness/test/specs/e2e.js
@@ -4,7 +4,7 @@ describe('Test Harness app', () => {
     before(() => {
         FlexnRunner.launchApp();
     });
-    it('scroll to Rows button and expect Rows button to be displayed by text', async () => {
+    it('--> scroll to Rows button and expect Rows button to be displayed by text', async () => {
         await FlexnRunner.waitForDisplayedById('flexn-screens-home-test-case-list-button-0-0');
         if (process.env.PLATFORM === 'web' || process.env.PLATFORM === 'macos') {
             await FlexnRunner.scrollById('flexn-screens-home-test-case-list-button-1-0', 'down', 'flexn-screens-home-test-case-list-button-0-0');
