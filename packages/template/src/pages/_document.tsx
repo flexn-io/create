902848c3844c019ce7ed402cfb0f230b24c99d4f
Mihai Blaga <mihaiblaga89@gmail.com>
2022-09-14 09:52:03 +0300

    web fix

--- packages/template/src/pages/_document.tsx
+++ packages/template/src/pages/_document.tsx
@@ -1,18 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
-import { flush } from 'react-native-media-query';
-import { AppRegistry } from 'react-native-web';
 
 class MyDocument extends Document {
-    static async getInitialProps({ renderPage }) {
-        AppRegistry.registerComponent('Main', () => Main);
-        const { getStyleElement } = AppRegistry.getApplication('Main');
-        const { html, head } = renderPage();
-
-        const styles = [getStyleElement(), flush()];
-        return { html, head, styles: React.Children.toArray(styles) };
-    }
-
     render() {
         return (
             <Html lang="en">
