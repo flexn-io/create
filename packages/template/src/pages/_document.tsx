15aa3af96d8a0b0ffc67f8fdb4f892befd0a3252
kasinskas <kasinskas.r@gmail.com>
2022-09-14 11:26:32 +0300

    chore(Template): bring back rnmq flush

--- packages/template/src/pages/_document.tsx
+++ packages/template/src/pages/_document.tsx
@@ -1,7 +1,17 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
-
+import { flush } from 'react-native-media-query';
+import { AppRegistry } from 'react-native-web';
 class MyDocument extends Document {
+    static async getInitialProps({ renderPage }) {
+        AppRegistry.registerComponent('Main', () => Main);
+        const { getStyleElement } = AppRegistry.getApplication('Main');
+        const { html, head } = renderPage();
+
+        const styles = [getStyleElement(), flush()];
+        return { html, head, styles: React.Children.toArray(styles) };
+    }
+
     render() {
         return (
             <Html lang="en">
