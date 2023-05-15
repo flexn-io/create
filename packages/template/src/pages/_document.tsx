8531dc72cbc3b7715bb5959385c06f788ab6018c
Pavel Jacko <beherithrone@gmail.com>
2023-05-15 03:38:35 +0200

    update deps

--- packages/template/src/pages/_document.tsx
+++ packages/template/src/pages/_document.tsx
@@ -1,15 +1,17 @@
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React from 'react';
 import { flush } from 'react-native-media-query';
 import { AppRegistry } from 'react-native-web';
 class MyDocument extends Document {
-    static async getInitialProps({ renderPage }) {
+    static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
         AppRegistry.registerComponent('Main', () => Main);
         const { getStyleElement } = AppRegistry.getApplication('Main');
-        const { html, head } = renderPage();
+        const initialProps = await Document.getInitialProps(ctx);
+        const { html, head } = await ctx.renderPage();
 
         const styles = [getStyleElement(), flush()];
-        return { html, head, styles: React.Children.toArray(styles) };
+        return { ...initialProps, html, head, styles: React.Children.toArray(styles) };
     }
 
     render() {
@@ -17,6 +19,7 @@ class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta name="description" content="Flexn Create Example" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
                     <link rel="shortcut icon" href="/favicon.ico" />
                 </Head>
                 <body>
