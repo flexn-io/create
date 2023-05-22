7dc1e54f641c868b7644cc44709a708e2cbb01e0
aurimasmi <aurimasmickys@gmail.com>
2023-05-22 14:16:19 +0300

    chore: upgrade plugins and flash-list

--- packages/app-harness/src/navigation/index.tsx
+++ packages/app-harness/src/navigation/index.tsx
@@ -19,7 +19,7 @@ const RootStack = createNativeStackNavigator();
 const Navigation = () => {
     return (
         <NavigationContainer>
-            <RootStack.Navigator screenOptions={{ headerShown: false }} initialRouteName="PressableAnimations">
+            <RootStack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Selector">
                 <RootStack.Screen name="Selector" component={Selector} />
                 <RootStack.Screen name="Row" component={Row} />
                 <RootStack.Screen name="DynamicState" component={DynamicState} />
