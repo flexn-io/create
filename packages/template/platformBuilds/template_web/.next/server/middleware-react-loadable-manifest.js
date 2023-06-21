409d062691946fb5d3e16ecfaeb6800af09d7bc1
aurimasmi <aurimasmickys@gmail.com>
2023-06-21 11:01:56 +0300

    chore: remove leftovers from template

--- packages/template/platformBuilds/template_web/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST={"../../../../node_modules/next/dist/client/index.js -> ../pages/_error":{"id":"../../../../node_modules/next/dist/client/index.js -> ../pages/_error","files":["static/chunks/node_modules_next_dist_pages__error_js.js"]}}
