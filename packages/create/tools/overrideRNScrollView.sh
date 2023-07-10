3f89e1989a6b13fc239da44d21925e277b528365
aurimasmi <aurimasmickys@gmail.com>
2023-07-10 16:07:46 +0300

    chore: use sed instead whole file override

--- packages/create/tools/overrideRNScrollView.sh
+++ packages/create/tools/overrideRNScrollView.sh
@@ -6,7 +6,7 @@ CREATE_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SO
 RN_DIR="${CREATE_DIR}/../../../react-native/Libraries/Components/ScrollView"
 
 if [ -e $RN_DIR ]; then
-    cp "${CREATE_DIR}/ScrollViewNativeComponent.js" "${RN_DIR}/ScrollViewNativeComponent.js"
+    sed -i '' "s|'RCTScrollView'|Platform.isTV \&\& Platform.OS === 'android' ? 'RCTScrollViewTV' : 'RCTScrollView'|" "${RN_DIR}/ScrollViewNativeComponent.js"
     echo "Overriding done."
 else
     echo "React native directory does not exists."
