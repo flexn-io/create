83d33085098d22c47e4eb5b7cbf8137fbfa4174f
aurimasmi <aurimasmickys@gmail.com>
2023-07-10 12:02:32 +0300

    fix: override script

--- packages/create/tools/overrideRNScrollView.sh
+++ packages/create/tools/overrideRNScrollView.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 CREATE_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
 RN_DIR="${CREATE_DIR}/../../../react-native/Libraries/Components/ScrollView"
 
-cp "${CREATE_DIR}/ScrollViewNativeComponent.js" "${RN_DIR}/ScrollViewNativeComponent.js" 2>/dev/null
-
-echo "Overriding done."
+if [ -e $RN_DIR ]; then
+    cp "${CREATE_DIR}/ScrollViewNativeComponent.js" "${RN_DIR}/ScrollViewNativeComponent.js"
+    echo "Overriding done."
+else
+    echo "React native directory does not exists."
+fi
