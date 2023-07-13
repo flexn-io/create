9317abdd2ff9264e26f2a19ee870454be374e9b0
aurimasmi <aurimasmickys@gmail.com>
2023-07-13 14:44:19 +0300

    fix: apply override only from rn 0.68

--- packages/create/tools/overrideRNScrollView.sh
+++ packages/create/tools/overrideRNScrollView.sh
@@ -3,11 +3,18 @@
 set -euo pipefail
 
 CREATE_DIR=$(cd -P "$(dirname "$(readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")" && pwd)
-RN_DIR="${CREATE_DIR}/../../../react-native/Libraries/Components/ScrollView"
+RN_DIR="${CREATE_DIR}/../../../react-native"
+RN_SCROLLVIEW_DIR="${RN_DIR}/Libraries/Components/ScrollView"
 
-if [ -e $RN_DIR ]; then
-    sed -i '' "s|'RCTScrollView'|Platform.isTV \&\& Platform.OS === 'android' ? 'RCTScrollViewTV' : 'RCTScrollView'|" "${RN_DIR}/ScrollViewNativeComponent.js"
+PACKAGE_VERSION=$(cat ${RN_DIR}/package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{print $2 }' \
+  | sed 's/[",]//g')
+
+if [ -e $RN_SCROLLVIEW_DIR ] && [ "${PACKAGE_VERSION:3:2}" -ge "68" ] ; then
+    sed -i '' "s|'RCTScrollView'|Platform.isTV \&\& Platform.OS === 'android' ? 'RCTScrollViewTV' : 'RCTScrollView'|" "${RN_SCROLLVIEW_DIR}/ScrollViewNativeComponent.js"
     echo "Overriding done."
 else
-    echo "React native directory does not exists."
+    echo "React native directory does not exists or react-native version is lover than 0.68"
 fi
