1e7eca0c0f5c23c773a6f7d335f133b8139c9150
aurimasmi <aurimasmickys@gmail.com>
2023-08-15 20:07:44 +0300

    fix: elements with translate measuring after animation

--- packages/create/src/apis/Animated/index.ts
+++ packages/create/src/apis/Animated/index.ts
@@ -1,3 +1,126 @@
 import { Animated } from 'react-native';
+import { measureSync } from '../../focusManager/layoutManager';
+import FocusModel from '../../focusManager/model/abstractFocusModel';
+import CoreManager from '../../focusManager/service/core';
 
-export default Animated;
+type EndResult = { finished: boolean };
+type EndCallback = (result: EndResult) => void;
+
+const onAnimationEnds = () => {
+    Object.values(CoreManager.getViews()).forEach((model: FocusModel) => {
+        if (model.isInForeground()) {
+            measureSync({ model });
+        }
+    });
+};
+
+export const loop = (
+    animation: Animated.CompositeAnimation,
+    config: Animated.LoopAnimationConfig
+): Animated.CompositeAnimation => {
+    return {
+        start: (callback?: EndCallback) => {
+            Animated.loop(animation, config).start(({ finished }) => {
+                callback?.({ finished });
+                onAnimationEnds();
+            });
+        },
+        stop: Animated.loop(animation, config).stop,
+        reset: Animated.loop(animation, config).reset,
+    };
+};
+
+export const stagger = (time: number, animations: Array<Animated.CompositeAnimation>): Animated.CompositeAnimation => {
+    return {
+        start: (callback?: EndCallback) => {
+            Animated.stagger(time, animations).start(({ finished }) => {
+                callback?.({ finished });
+                onAnimationEnds();
+            });
+        },
+        stop: Animated.stagger(time, animations).stop,
+        reset: Animated.stagger(time, animations).reset,
+    };
+};
+
+export const parallel = (
+    animations: Array<Animated.CompositeAnimation>,
+    config: Animated.ParallelConfig
+): Animated.CompositeAnimation => {
+    return {
+        start: (callback?: EndCallback) => {
+            Animated.parallel(animations, config).start(({ finished }) => {
+                callback?.({ finished });
+                onAnimationEnds();
+            });
+        },
+        stop: Animated.parallel(animations, config).stop,
+        reset: Animated.parallel(animations, config).reset,
+    };
+};
+
+export const sequence = (animations: Array<Animated.CompositeAnimation>): Animated.CompositeAnimation => {
+    return {
+        start: (callback?: EndCallback) => {
+            Animated.sequence(animations).start(({ finished }) => {
+                callback?.({ finished });
+                onAnimationEnds();
+            });
+        },
+        stop: Animated.sequence(animations).stop,
+        reset: Animated.sequence(animations).reset,
+    };
+};
+
+export const spring = (
+    value: Animated.Value | Animated.ValueXY,
+    config: Animated.SpringAnimationConfig
+): Animated.CompositeAnimation => {
+    return {
+        start: (callback?: EndCallback) => {
+            Animated.spring(value, config).start(({ finished }) => {
+                callback?.({ finished });
+                onAnimationEnds();
+            });
+        },
+        stop: Animated.spring(value, config).stop,
+        reset: Animated.spring(value, config).reset,
+    };
+};
+
+export const decay = (
+    value: Animated.Value | Animated.ValueXY,
+    config: Animated.DecayAnimationConfig
+): Animated.CompositeAnimation => {
+    return {
+        start: (callback?: EndCallback) => {
+            Animated.decay(value, config).start(({ finished }) => {
+                callback?.({ finished });
+                onAnimationEnds();
+            });
+        },
+        stop: Animated.decay(value, config).stop,
+        reset: Animated.decay(value, config).reset,
+    };
+};
+
+export const timing = (
+    value: Animated.Value | Animated.ValueXY,
+    config: Animated.TimingAnimationConfig
+): Animated.CompositeAnimation => {
+    return {
+        start: (callback?: EndCallback) => {
+            Animated.timing(value, config).start(({ finished }) => {
+                callback?.({ finished });
+                onAnimationEnds();
+            });
+        },
+        stop: Animated.timing(value, config).stop,
+        reset: Animated.timing(value, config).reset,
+    };
+};
+
+export default {
+    ...Animated,
+    timing,
+};
--- packages/create/src/hooks/useOnLayout.ts
+++ packages/create/src/hooks/useOnLayout.ts
@@ -1,35 +1,18 @@
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { InteractionManager } from 'react-native';
 import CoreManager from '../focusManager/service/core';
 import Event, { EVENT_TYPES } from '../focusManager/events';
 import FocusModel from '../focusManager/model/abstractFocusModel';
 
 export default function useOnLayout(model: FocusModel | null, callback?: (() => void) | (() => Promise<void>)) {
-    const interactionPromise = useRef<Promise<any>>();
-    const pendingCallbacks = useRef<{ (): void | Promise<void> }[]>([]).current;
-
     useEffect(() => {
-        interactionPromise.current = InteractionManager.runAfterInteractions(() => {
-            if (pendingCallbacks.length) {
-                for (let index = 0; index < pendingCallbacks.length; index++) {
-                    pendingCallbacks[index]();
-                }
-                pendingCallbacks.splice(0, pendingCallbacks.length);
-            }
-
-            return true;
-        }).then(() => Promise.resolve());
+        InteractionManager.runAfterInteractions(() => {
+            sendOnLayoutEvent();
+        });
     }, []);
 
     const onLayout = () => {
-        if (interactionPromise.current) {
-            sendOnLayoutEvent();
-            interactionPromise.current.then(() => {
-                sendOnLayoutEvent();
-            });
-        } else {
-            if (callback) pendingCallbacks.push(callback);
-        }
+        sendOnLayoutEvent();
     };
 
     const sendOnLayoutEvent = () => {
