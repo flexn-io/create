7650c2f015b9f326036db3c0fd3d1df1608bce23
pauliusguzas <paulius.guzas@flexn.io>
2023-09-25 11:46:49 +0300

    chore: update row tests and organize tests

---
+++ packages/app-harness/test/specs/R1.cjs
@@ -0,0 +1,65 @@
+const FlexnRunner = require('@flexn/graybox').default;
+
+describe('R1', () => {
+    before(() => {
+        FlexnRunner.launchApp();
+    });
+    it('Check that row test is displayed in home page', async () => {
+        await FlexnRunner.expectToBeDisplayedById('home-R1');
+    });
+
+    it('Focus on row test', async () => {
+        await FlexnRunner.pressButtonRight(1);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'home-R1');
+    });
+
+    it('Open row test', async () => {
+        await FlexnRunner.pressButtonSelect(1);
+        await FlexnRunner.expectToBeDisplayedByText('#R1 ');
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'R1-0');
+    });
+
+    it('Scroll row to the end', async () => {
+        for (let i = 1; i <= 19; i++) {
+            await FlexnRunner.pressButtonRight(1);
+            await FlexnRunner.expectToHaveTextById('focused-element-selector', `R1-${i}`);
+        }
+    });
+
+    it('Focus is on last item and click right', async () => {
+        await FlexnRunner.pressButtonRight(1);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'R1-19');
+    });
+
+    it('Focus is on last item and click up', async () => {
+        await FlexnRunner.pressButtonUp(1);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'R1-19');
+    });
+
+    it('Focus is on last item and click down', async () => {
+        await FlexnRunner.pressButtonDown(1);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'R1-19');
+    });
+
+    it('Scroll row to the beginning', async () => {
+        for (let i = 18; i >= 0; i--) {
+            await FlexnRunner.pressButtonLeft(1);
+            await FlexnRunner.expectToHaveTextById('focused-element-selector', `R1-${i}`);
+        }
+    });
+
+    it('Focus is on first item and click left', async () => {
+        await FlexnRunner.pressButtonLeft(1);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'R1-0');
+    });
+
+    it('Focus is on first item and click up', async () => {
+        await FlexnRunner.pressButtonUp(1);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'R1-0');
+    });
+
+    it('Focus is on first item and click down', async () => {
+        await FlexnRunner.pressButtonDown(1);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'R1-0');
+    });
+});
---
+++ packages/app-harness/test/specs/RH1.cjs
@@ -0,0 +1,45 @@
+const FlexnRunner = require('@flexn/graybox').default;
+
+describe('RH1', () => {
+    before(() => {
+        FlexnRunner.launchApp();
+    });
+    it('Check that remote handler test is displayed in home page', async () => {
+        await FlexnRunner.expectToBeDisplayedById('home-RH1');
+    });
+
+    it('Focus on remote handler test', async () => {
+        await FlexnRunner.pressButtonRight(4);
+        await FlexnRunner.expectToHaveTextById('focused-element-selector', 'home-RH1');
+    });
+
+    it('Open remote handler test', async () => {
+        await FlexnRunner.pressButtonSelect(1);
+        await FlexnRunner.expectToBeDisplayedByText('#RH1 ');
+    });
+
+    it('Click up button', async () => {
+        await FlexnRunner.pressButtonUp(1);
+        await FlexnRunner.expectToHaveTextById('RH1-pressed-button', 'You pressed up');
+    });
+
+    it('Click down button', async () => {
+        await FlexnRunner.pressButtonDown(1);
+        await FlexnRunner.expectToHaveTextById('RH1-pressed-button', 'You pressed down');
+    });
+
+    it('Click left button', async () => {
+        await FlexnRunner.pressButtonLeft(1);
+        await FlexnRunner.expectToHaveTextById('RH1-pressed-button', 'You pressed left');
+    });
+
+    it('Click right button', async () => {
+        await FlexnRunner.pressButtonRight(1);
+        await FlexnRunner.expectToHaveTextById('RH1-pressed-button', 'You pressed right');
+    });
+
+    it('Click select button', async () => {
+        await FlexnRunner.pressButtonSelect(1);
+        await FlexnRunner.expectToHaveTextById('RH1-pressed-button', 'You pressed select');
+    });
+});
