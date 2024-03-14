23dd6589465231d954d5d2af419ac81f57d18181
Mihai Blaga <mihaiblaga89@gmail.com>
2024-03-14 16:00:40 +0100

    added message for rnvi workaround

--- packages/template-starter/react-native.config.js
+++ packages/template-starter/react-native.config.js
@@ -3,6 +3,7 @@ const { withRNVRNConfig } = require('rnv');
 
 const config = withRNVRNConfig({
     dependencies: {
+        // !TODO workaround for ios. This should be removed once it's properly fixed. tracked here https://github.com/flexn-io/create/issues/195
         'react-native-vector-icons': {
             platforms: {
                 ios: null,
