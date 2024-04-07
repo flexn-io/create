f876050648549c63657007a0bcb67888f567b474
Mihai Blaga <mihaiblaga89@gmail.com>
2024-04-07 23:12:47 +0300

    migrate hook, added simple-git devDep

--- buildHooks/src/git.js
+++ buildHooks/src/git.js
@@ -1,8 +1,6 @@
 import path from 'path';
-import { FileUtils, Logger } from '@rnv/core';
+import { logHook, readObjectSync } from '@rnv/core';
 import simpleGit from 'simple-git';
-
-const { readObjectSync } = FileUtils;
 
 export const gitCommit = async (c) => {
     const baseDir = c.paths.project.dir;
@@ -11,13 +9,13 @@ export const gitCommit = async (c) => {
 
     const { version } = rootPkgFile;
 
-    Logger.logHook(`gitCommitAndTagVersion ${version}`);
+    logHook(`gitCommitAndTagVersion ${version}`);
     const git = simpleGit({ baseDir });
-    Logger.logHook('adding files');
+    logHook('adding files');
     await git.add(`${baseDir}/*`);
-    Logger.logHook('COMMITING...');
+    logHook('COMMITING...');
     await git.commit(`chore(release): publish ${version}`);
-    Logger.logHook('DONE');
+    logHook('DONE');
     return true;
 };
 
@@ -27,7 +25,7 @@ export const gitTag = async (c) => {
     const rootPkgFile = readObjectSync(path.join(baseDir, 'lerna.json'));
     const { version } = rootPkgFile;
 
-    Logger.logHook(`gitTagAndPush ${version}`);
+    logHook(`gitTagAndPush ${version}`);
     const git = simpleGit({ baseDir });
     await git.addTag(`${version}`);
     await git.push();
