add0a6a138f7c992be37200b8cb6263482579e23
aurimasmi <aurimasmickys@gmail.com>
2024-04-10 20:52:44 +0300

    fix: ios compilation without disabling create module

--- packages/create/ios/TvFocusableViewManager.m
+++ packages/create/ios/TvFocusableViewManager.m
@@ -1,11 +1,22 @@
 #import "React/RCTViewManager.h"
+#if TARGET_OS_TV
 #import "React/RCTTVView.h"
+#else
+#import <React/RCTView.h>
+#endif
 
+#if TARGET_OS_TV
 @interface TvFocusableView : RCTTVView
     @property (nonatomic, assign) NSDictionary *animatorOptions;
     @property (nonatomic, assign) BOOL *isTVSelectable;
 @end
+#else
+@interface TvFocusableView : RCTView
 
+@end
+#endif
+
+#if TARGET_OS_TV
 @interface RCT_EXTERN_MODULE(TvFocusableViewManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(animatorOptions, NSDictionary)
@@ -19,3 +30,4 @@ RCT_EXTERN_METHOD(
 )
 
 @end
+#endif
