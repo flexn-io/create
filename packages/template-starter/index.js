a1c34e9aa11f12691c3f95d719edeac8ab09852a
aurimasmi <aurimasmickys@gmail.com>
2024-04-11 09:52:03 +0300

    chore: upgrade rnv to rc.16

--- packages/template-starter/index.js
+++ packages/template-starter/index.js
@@ -1,27 +1,6 @@
 import App from './src/app';
 import { renderApp } from '@flexn/create';
-// import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
-// import { Text, View, NativeModules } from 'react-native';
-
-// const NativeComponent = codegenNativeComponent('RNSScreenStackHeaderConfig', {});
-// const Activity = codegenNativeComponent('AndroidProgressBar', {
-//     interfaceOnly: true,
-// });
-
-// const { AndroidProgressBar, RNSScreenStackHeaderConfig } = NativeModules;
-
-// console.log('NativeComponent', NativeComponent, typeof NativeComponent, RNSScreenStackHeaderConfig);
-// console.log('Activity', Activity, typeof Activity, AndroidProgressBar);
-
-// const App = () => {
-//     return (
-//         <View style={{ flex: 1, backgroundColor: 'red' }}>
-//             <Text>Hello Red</Text>
-//             <NativeComponent />
-//             <Activity />
-//         </View>
-//     );
-// };
 
 const Main = renderApp(App);
+
 export default Main;
