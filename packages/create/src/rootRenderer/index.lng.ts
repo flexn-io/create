55dde225d8fd33e1c87789af5472963b2a864b91
aurimasmi <aurimasmickys@gmail.com>
2024-04-11 11:17:59 +0300

    chore: remove lightningjs

--- packages/create/src/rootRenderer/index.lng.ts
+++
@@ -1,7 +0,0 @@
-import { Lightning, Launch, PlatformSettings, AppData } from '@lightningjs/sdk';
-
-export const renderApp = (App: any) => {
-    const Main = (appSettings: Lightning.Application.Options, platformSettings: PlatformSettings, appData: AppData) =>
-        Launch(App, appSettings, platformSettings, appData);
-    return Main;
-};
