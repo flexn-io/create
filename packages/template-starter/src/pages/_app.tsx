0477200ea6e37d18c39b3b3493e464f4bd9e7163
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2024-09-20 15:40:51 +0300

    Revert "update rnv version + deps"

--- packages/template-starter/src/pages/_app.tsx
+++ packages/template-starter/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Debugger, App } from '@flexn/create';
 import Menu from '../components/menu';
 import { themeStyles, ThemeProvider } from '../config';
@@ -6,25 +6,18 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps<any>) {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
     return (
         <>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
-            {isClient ? (
-                <ThemeProvider>
-                    <Menu />
-                    <App style={themeStyles.appContainer}>
-                        <Component {...pageProps} />
-                        <Debugger />
-                    </App>
-                </ThemeProvider>
-            ) : null}
+            <ThemeProvider>
+                <Menu />
+                <App style={themeStyles.appContainer}>
+                    <Component {...pageProps} />
+                    <Debugger />
+                </App>
+            </ThemeProvider>
         </>
     );
 }
